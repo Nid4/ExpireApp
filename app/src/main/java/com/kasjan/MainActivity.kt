44f9523df8b92d66c3a589b97d14bf6e5d67e2bd
Noxid <noxidpl@gmail.com>
2024-12-19 00:04:22 +0100

    New menu navigation. Now is static 3 fragments in menu. 1. Fragment day - no changes (RecycleView with products) 2. Fragment Days List - (TODO RecycleView for data ) 3. Fragment Settings - (TODO Option delete accounts etc.)

--- app/src/main/java/com/kasjan/MainActivity.kt
+++ app/src/main/java/com/kasjan/MainActivity.kt
@@ -2,20 +2,11 @@ package com.kasjan
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import com.kasjan.adapter.DayMenuAdapter
 import com.kasjan.databinding.ActivityMainBinding
-import java.util.Calendar
-import java.util.Date
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,15 +15,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val viewPager = findViewById<ViewPager2>(R.id.viewpagerMain)
-        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+
         val adapter = ViewPagerAdapter(this)
-        viewPager.adapter = adapter
+        binding.viewpagerMain.adapter = adapter
 
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = "Dzień ${position + 1}" // Dynamiczne nazwy zakładek
+        TabLayoutMediator(binding.tabLayout, binding.viewpagerMain) { tab, position ->
+            when (position) {
+                0 -> tab.text = getString(R.string.tab_day)
+                1 -> tab.text = getString(R.string.tab_list)
+                2 -> tab.text = getString(R.string.tab_settings)
+            }
         }.attach()
-
 
 
     }
