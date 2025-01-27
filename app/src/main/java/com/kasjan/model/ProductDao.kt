f925f8230ae10fbde41f6ac00b3fff801b017f10
Noxid <noxidpl@gmail.com>
2025-01-27 09:12:42 +0100

    1.2.1 stable -add delete function

--- app/src/main/java/com/kasjan/model/ProductDao.kt
+++ app/src/main/java/com/kasjan/model/ProductDao.kt
@@ -1,18 +1,17 @@
 package com.kasjan.model
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
 import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
-import java.util.Date
 
 @Dao
 interface ProductDao {
+
     @Query("SELECT * FROM products WHERE day = :day AND month = :month AND year = :year")
     fun getProductsByDate(day: Int, month: Int, year: Int): List<ShopProduct>
-
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertProduct(product: ShopProduct)
--- app/src/main/java/com/kasjan/utils/FirebaseHelper.kt
+++ app/src/main/java/com/kasjan/utils/FirebaseHelper.kt
@@ -12,17 +12,29 @@ class FirebaseHelper {
         databaseReference.child(productId).setValue(product)
     }
 
-     fun deleteProductFromFirebase(product: ShopProduct) {
-         val productId = product.id.toString()
-         Log.d("FirebaseHelper", "Attempting to delete product with ID: $productId")
-        databaseReference.child(productId).removeValue()
-            .addOnSuccessListener {
-                Log.d("DayFragment", "Product deleted from Firebase: $productId")
-            }
-            .addOnFailureListener { exception ->
-                Log.e("DayFragment", "Failed to delete product from Firebase", exception)
-            }
-    }
+//     fun deleteProductFromFirebase(product: ShopProduct) {
+//         val productId = product.id.toString()
+//         Log.d("FirebaseHelper", "Attempting to delete product with ID: $productId")
+//        databaseReference.child(productId).removeValue()
+//            .addOnSuccessListener {
+//                Log.d("DayFragment", "Product deleted from Firebase: $productId")
+//            }
+//            .addOnFailureListener { exception ->
+//                Log.e("DayFragment", "Failed to delete product from Firebase", exception)
+//            }
+//    }
+fun deleteProductFromFirebase(product: ShopProduct) {
+    val productKey = product.ean.toString() // Zmień z id na ean
+    Log.d("FirebaseHelper", "Attempting to delete product with key: $productKey")
+    databaseReference.child(productKey).removeValue()
+        .addOnSuccessListener {
+            Log.d("DayFragment", "Product deleted from Firebase: $productKey")
+        }
+        .addOnFailureListener { exception ->
+            Log.e("DayFragment", "Failed to delete product from Firebase", exception)
+        }
+}
+
     // Pobierz produkty z Firebase
     fun getProductsFromFirebase(onSuccess: (List<ShopProduct>) -> Unit, onFailure: (Exception) -> Unit) {
         databaseReference.get().addOnSuccessListener { dataSnapshot ->
